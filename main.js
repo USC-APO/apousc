2407d04e086bc424bb504ed3b6df2af1ea44edae
Kelly Ma <kjma@usc.edu>
2022-02-11 11:46:45 -0800

    yay more code

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js'
 
 // Add Firebase products that you want to use
-import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-auth.js'
+import { getAuth, signInWithEmailAndPassword, setPersistence, browserSessionPersistence } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-auth.js'
+
 import { 
     collection, addDoc, setDoc, getDoc, updateDoc, deleteDoc, deleteField, getFirestore 
 } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js'
@@ -32,52 +33,53 @@ const analytics = getAnalytics(app);
 const database = getFirestore()
 const auth = getAuth(app)
 
-
-var signinBtn = document.getElementById("signin");
-if(signinBtn){
-        signinBtn. addEventListener("click", function(e){
-        e.preventDefault() //Prevent Default Form Submission Behavior
-        authenticate()
-    });
-}
-
-function authenticate(){
-    const email = document.getElementById("inputEmail").value
-    const password = document.getElementById("inputPassword").value
+// ====== SIGN IN ========
+document.querySelector("#signin").onclick = function(event){
+    event.preventDefault();
+    let email = document.getElementById("inputEmail").value
+    let password = document.getElementById("inputPassword").value
 
     signInWithEmailAndPassword(auth, email, password) 
         .then((userCredential) => {
-            // location.reload();
             // Signed in 
             var user = userCredential.user;
             console.log("user",user.email)
             window.location = "home.html";
         })
         .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // alert("error code", errorCode)
-            alert( errorMessage)
+            //cannot sign in
+            document.querySelector(".error-message").innerHTML = "Password or email is incorrect. Please try again.";
+            document.querySelector(".error-message").classList.remove("hide");
         });
 }
 
+// ====== Events Page ========
 async function addEvent(){ //with auto-id
-    var eventName = document.getElementById("event-name").value
-    var eventLoc = document.getElementById("event-location").value
-    console.log(eventName)
+    var eventName = document.getElementById("event-name").value.trim();
+    var eventLoc = document.getElementById("event-location").value.trim();
+    if(eventName.length < 1 || eventLoc.length < 1){
+        document.querySelector(".fail-message").style.display = "block";
+        document.querySelector(".fail-message").style.color = "red";
+    }
+    else{
+        var ref = collection(database,'events');
+        var docRef = await addDoc(ref, {
+                Name: eventName,
+                Location: eventLoc
+            }
+        )
+        .then(()=>{
+            document.querySelector(".success-message").style.display = "block";
+            document.querySelector(".success-message").style.color = "red";
+            // alert("data added successfully");
+        })
+        .catch((error)=>{
+            document.querySelector(".fail-message").style.display = "block";
+            document.querySelector(".fail-message").style.color = "red";
+            // alert("unsuccessful");
+        });
+    }
 
-    var ref = collection(database,'events');
-    var docRef = await addDoc(ref, {
-            Name: eventName,
-            Location: eventLoc
-        }
-    )
-    .then(()=>{
-        alert("data added successfully");
-    })
-    .catch((error)=>{
-        alert("unsuccessful");
-    });
 
 }
 
